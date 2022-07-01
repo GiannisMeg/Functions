bed0664506972def4f81c1e40f58ec000de43666
GiannisMeg <megoulas2@hotmail.com>
2022-07-01 12:19:29 +0200

    first function commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function double1() {
+	console.log(arguments[0]);
+}
+double1(3 * 2);
+
+// function double2() {
+// 	console.log(double2);
+// 	return const mult = num1 *2 ;
+// }
+
+// arrow_double2(() => arguments[0] * 2);
+// arrow_double3((arguments) => num1 > num2?num1 * 2:num2 * 2);
+
+// function double5() {}
+// double5();
+
+// function double6() {}
+// double6();
+
+// function double7() {}
+// double7();
+
+// const numberEvenBelow = (number) => {
+// 	let arr = [];
+// 	for (let i = 1; i < number; i++) {
+// 		if (i % 2 === 0) {
+// 			arr.push(i);
+// 		}
+// 	}
+// 	console.log(arr);
+// };
+// numberEvenBelow(10);
